4938cea403411fae35bc4f1fe1c481246312b7cc
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-02-16 23:07:40 +0800

    Update and rename PrimeNemberChecker.java to PrimeNumberChecker.java

--- JAVA/PrimeNumberChecker.java
+++ JAVA/PrimeNumberChecker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimeNemberChecker {
+public class PrimeNumberChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -13,7 +13,6 @@ public class PrimeNemberChecker {
             for (int i = 2; i * i <= num; i++) {
                 if (num % i == 0) {
                     isPrime = false;
-                    break;
                 }
             }
         }
