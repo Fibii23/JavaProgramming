e1214940917a8d900fe522cc8c43d5431b45dcb8
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-03-10 07:20:59 +0800

    Create FitnessApp.java

---
+++ JAVA/FitnessApp.java
@@ -0,0 +1,21 @@
+public class FitnessApp {
+
+    public static String calculateBMI(double weight, double height) {
+        double bmi = weight / (height * height);
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "Normal";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
+    public static void main(String[] args) {
+        double weight = 70;
+        double height = 1.75;
+        System.out.println("BMI Category: " + calculateBMI(weight, height));
+    }
+}
