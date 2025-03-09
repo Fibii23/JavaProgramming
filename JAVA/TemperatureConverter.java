2004a4e13b3357beafb8f8ad224990f46406d2e6
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-03-10 07:23:15 +0800

    Update TemperatureConverter.java

--- JAVA/TemperatureConverter.java
+++ JAVA/TemperatureConverter.java
@@ -1,9 +1,17 @@
 public class TemperatureConverter {
-    public static void main(String[] args) {
-        double celsiusTemp = 25.0;
-        double fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
 
-        System.out.println("Temperature in Celsius: " + celsiusTemp);
-        System.out.println("Temperature in Fahrenheit: " + fahrenheitTemp);
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = 77;
+        System.out.println(celsius + "°C = " + celsiusToFahrenheit(celsius) + "°F");
+        System.out.println(fahrenheit + "°F = " + fahrenheitToCelsius(fahrenheit) + "°C");
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
     }
 }
