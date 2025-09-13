70a42c02d1c882aa768db345d43928c718cb4fd2
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-09-13 16:40:34 +0800

    Create MidtermAct2.java

---
+++ MidtermAct2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MidtermAct2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int difference = 0;
+        int product = 1;
+        double quotient = 1.0;
+
+        boolean firstEven = true; 
+
+        System.out.println("Enter 20 numbers:");
+
+        for (int i = 1; i <= 20; i++) {
+            int num = scanner.nextInt();
+
+            if (num % 2 == 0 && num < 100) {
+                sum += num; 
+                product *= num; 
+
+                if (firstEven) {
+                    difference = num;
+                    quotient = num;
+                    firstEven = false;
+                } else {
+                    difference -= num;
+                    quotient /= num;
+                }
+            }
+        }
+
+        System.out.println("Sum of even numbers < 100: " + sum);
+        System.out.println("Difference of even numbers < 100: " + difference);
+        System.out.println("Product of even numbers < 100: " + product);
+        System.out.println("Quotient of even numbers < 100: " + quotient);
+    }
+}
