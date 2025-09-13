7365b83561d8777256934df7729af7af8c530166
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-09-13 11:49:26 +0800

    Create PrelimAct8.java

---
+++ PrelimAct8.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BankLoanEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Employment Status (1=Employed, 2=Self-Employed, 3=Unemployed): ");
+        int status = sc.nextInt();
+
+        System.out.println("Enter Salary: ");
+        double salary = sc.nextDouble();
+
+        switch (status) {
+            case 1: 
+                if (salary >= 30000) {
+                    System.out.println("Loan Approved");
+                } else {
+                    System.out.println("Loan Denied");
+                }
+                break;
+
+            case 2: 
+                if (salary >= 50000) {
+                    System.out.println("Loan Approved");
+                } else {
+                    System.out.println("Loan Denied");
+                }
+                break;
+
+            case 3: 
+                System.out.println("Loan Denied");
+                break;
+
+            default:
+                System.out.println("Invalid Employment Status");
+        }
+    }
+}
