1ddfbf11c4927c806917374367c089566e8ca424
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-09-15 19:59:28 +0800

    Update PrelimAct10.java

--- PrelimAct10.java
+++ PrelimAct10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EmployeeOvertimePay {
+public class PrelimAct10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
