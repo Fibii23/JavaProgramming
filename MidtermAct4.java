85d4695a3d16be79cd96eed67ab8425b6dff5cae
Fibii23 <149024836+Fibii23@users.noreply.github.com>
2025-09-20 17:03:36 +0800

    Update MidtermAct4.java

--- MidtermAct4.java
+++ MidtermAct4.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-public class RepeatCalculator {
+public class MidtermAct4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  // Scanner for user input
+        Scanner input = new Scanner(System.in);  
 
         String choice = "yes"; 
 
-        while (choice.equalsIgnoreCase("yes")) {  
+        while (!choice.isEmpty() && (choice.charAt(0) == 'y' || choice.charAt(0) == 'Y')) {  
             
             System.out.print("Enter first number: ");
-            double num1 = sc.nextDouble();
+            double num1 = input.nextDouble();
 
             System.out.print("Enter second number: ");
-            double num2 = sc.nextDouble();
+            double num2 = input.nextDouble();
 
             System.out.print("Enter operation (+, -, *, /): ");
-            char op = sc.next().charAt(0);
+            char operator = input.next().charAt(0);
 
             double result; 
 
-    
-            switch (op) {
+            switch (operator) {
                 case '+':
                     result = num1 + num2;
                     System.out.println("Result: " + result);
@@ -50,7 +49,7 @@ public class RepeatCalculator {
             }
 
             System.out.print("Do you want to calculate again? (yes/no): ");
-            choice = sc.next();  
+            choice = input.next();  
         }
 
         System.out.println("Calculator closed. Goodbye!");
